ce0e4e8e29c741af98c7441eb5e7e4e63bad26fd
tung0973 <71159909+tung0973@users.noreply.github.com>
2020-09-24 01:03:14 +0700

    Assignment 5 !!!

---
+++ Ass5.1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int n;
+	do{
+	
+		printf("xin moi nhap n =");
+		scanf("%d",&n);
+		
+	}while(n<0);
+
+		for(int i=1;i<=n;i++){
+			if(i%2==0){
+			printf("\n so chan nho hon n la ");
+			printf("%d",i);
+			}
+	
+	}
+
+	
+	}	
+		
+
---
+++ Ass5.2.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+	int n;
+	int s=0;
+	do{
+	
+		printf("xin moi nhap n =");
+		scanf("%d",&n);
+	if(n<0){
+		printf("xin vui long nhap lai !!!!");
+	}
+		
+	}while(n<1);
+	printf(" uoc so cua n la:");
+		for(int i=1;i<=n;i++){
+			if(n%i==0){
+				printf("%d,",i);
+				
+				}
+				
+			}
+			}
+			
+		
+	
+	
+	
+
+	
+	
